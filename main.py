fbfcbf5cd3b97f614b45372decedbd40a3b0163b
Yana95 <79191411745@yandex.ru>
2020-09-06 16:32:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+file_path = 'TEST2.txt'
+
+files = {'file': open(file_path, 'rb')}
+
+
+header = {'Authorization' : 'Введите токен'}
+
+
+def upload():
+  disc = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload?path=file_path', headers = header)
+  href = (disc.json()['href'])
+  response = requests.put(href, files=files, headers=header)
+  print(f'Ваш {file_path} успешно загружен')
+
+
+upload()
